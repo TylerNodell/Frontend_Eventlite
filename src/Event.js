a4179dfc4043c174e092c7234011919a728deb0f
TylerNodell <Tyler.l.nodell@gmail.com>
2018-09-11 10:38:05 -0400

    added stuff

---
+++ src/Event.js
@@ -0,0 +1,8 @@
+class Event {
+  constructor(name, image, date, description) {
+    this.name = name;
+    this.image = image;
+    this.date = date;
+    this.description = description;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   const eventlist = document.getElementById("events_list")
-  // debugger
-  // fetchData(BASE_URL)
-  fetch(BASE_URL)
-  .then((resp) => resp.json())
-  .then((data) => {
-    // debugger
-    console.log(typeof data)
-    generateEvents(data, eventlist)
-  })
-  
-  
-  
+  const topbar = document.getElementById('top_bar')
+
+  fetchData(BASE_URL, eventlist)
 })
 
 
@@ -19,15 +10,15 @@ const BASE_URL = "http://localhost:3000/events"
 // const eventData = fetchData(BASE_URL)
 
 
-function fetchData(url) {
+function fetchData(url, eventlist) {
   fetch(url)
   .then((resp) => resp.json())
   .then((data) => {
     console.log(data)
-    generateEvents(data)
+    generateEvents(data, eventlist)
   })
-  // debugger
 }
+
 
 function generateEvents(data, eventlist) {
   // Iterating through each event
@@ -52,7 +43,63 @@ function generateEvents(data, eventlist) {
     eventContainer.appendChild(eventName)
     eventContainer.appendChild(eventDate)
     // Append the card div to the event list
-    // debugger
     eventlist.appendChild(eventContainer)
   })
 }
+// User generated event
+function createEvent() {
+  // Build form div
+  let formDiv = document.createElement('div')
+  formDiv.id = "formdiv"
+  // Build form
+  let form = document.createElement('form')
+  form.id = "event-creation-form"
+  // Create Labels
+  let formName = document.createElement('label')
+  forNname.className = "form-label"
+  let formImage = document.createElement('label')
+  formImage.className = "form-label"
+  let formDate = document.createElement('label')
+  formDate.className = "form-label"
+  let formDescription = document.createElement('label')
+  formDescription.className = "form-label"
+  // Create inputs
+  let formNameInput = document.createElement('input')
+  formNameInput.type = "text"
+  formNameInput.className = "form-input"
+  let formImageInput = document.createElement('input')
+  formImageInput.type = "text"
+  formImageInput.className = "form-input"
+  let formDateInput = document.createElement('input')
+  formDateInput.type = "text"
+  formDateInput.className = "form-input"
+  let formDescriptionInput = document.createElement('input')
+  formDescriptionInput.type = "text"
+  formDescriptionInput.className = "form-input"
+  // Create submit button
+  let formsubmission = document.createElement('input')
+  formsubmission.type = "submit"
+  // Bring form together
+  form.appendChild(formName)
+  form.appendChild(formNameInput)
+  form.appendChild(formImage)
+  form.appendChild(formImageInput)
+  form.appendChild(formDate)
+  form.appendChild(formDateInput)
+  form.appendChild(formDescription)
+  form.appendChild(formDescriptionInput)
+  // Append form to formDiv
+  formDiv.appendChild(form)
+  // Append formDiv to document
+  document.appendChild(formDiv)
+
+  // Add values to Event
+  let newEvent = new Event(name, image, date, description)
+  
+}
+
+// eventlist{
+//   mainpage = document.getElementById('main-page')
+//   mainpage.style.visibility = 'hidden'
+//   eventpage.style.visibility = 'visible'
+// }
